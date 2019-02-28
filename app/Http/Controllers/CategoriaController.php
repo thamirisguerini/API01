0a4ae5954ff67e250ebf90a60950a64afe12b5e2
ALUNO <thamirisaguerini@gmail.com>
2019-02-28 15:43:29 -0300

    first

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Categoria;
+
+class CategoriaController extends Controller
+{
+
+    public function _construct()
+    {
+        header('Access-Control-Allow-Origin: *');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categoria = Categoria::all();
+        return response()->json(['data'=>$categoria,'status'=>true]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $categoria = Categoria::create($data);
+
+        if($categoria)
+        {
+            return response()->json(['data'=>$categoria, 'status'=>true]);
+        }else{
+            return response()->json(['data'=>'Erro ao criar produto','status'=>false]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $categoria = Categoria::find($id);
+
+        if($categoria)
+        {
+            return response()->json(['data'=>$categoria, 'status'=>true]);
+        }else{
+            return response()->json(['data'=>'Produto não existe!','status'=>false]);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $categoria = Categoria::find($id);
+
+        if($categoria)
+        {
+            $categoria ->update($data);
+            return response()->json(['data'=>$categoria, 'status'=>true]);
+        }else{
+            return response()->json(['data'=>'Erro ao editar','status'=>false]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $categoria = Categoria::find($id);
+
+        if($categoria)
+        {
+            $categoria ->delete();
+            return response()->json(['data'=>'Deletado', 'status'=>true]);
+        }else{
+            return response()->json(['data'=>'Erro ao excluir','status'=>false]);
+        }
+    }
+}
